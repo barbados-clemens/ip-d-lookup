4f90a5b8347ecd136132177570e063880da01aa2
Caleb <caleb.ukle@yahoo.com>
2020-11-10 05:52:32 -0600

    add worker controller error handler

--- IpDLookUp.Worker/Controllers/WorkerController.cs
+++ IpDLookUp.Worker/Controllers/WorkerController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using IpDLookUp.Services;
 using IpDLookUp.Services.Models;
 using IpDLookUp.Services.Types;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -24,23 +25,38 @@ namespace IpDLookUp.Worker.Controllers
         public async Task<IActionResult> HandleServiceRequest([FromRoute] string address, ServiceType type)
         {
             _logger.LogInformation($"Processing {type} for {address}");
-            // not a fan of the controller having to know the specific
-            // types for search service
-            return type switch
+            try
             {
-                ServiceType.GeoIP => new OkObjectResult(await ServiceProcessor.Process<GeoIpModel>(address, type)),
-                ServiceType.RDAP => new OkObjectResult(await ServiceProcessor.Process<RdapModel>(address, type)),
-                ServiceType.ReverseDNS =>
-                    new OkObjectResult(await ServiceProcessor.Process<IPHostEntry>(address, type)),
-                ServiceType.SslLabs => new OkObjectResult(await ServiceProcessor.Process<SslLabsModel>(address, type)),
-                ServiceType.Ping => new OkObjectResult(await ServiceProcessor.Process<PingModel>(address, type)),
-                _ => new BadRequestObjectResult(new ServiceResult<string>
+                // not a fan of the controller having to know the specific
+                // types for search service
+                return type switch
                 {
-                    Status = ServiceStatus.Bad,
-                    Type = type,
-                    ErrorMessage = $"Invalid type passed in. Expected a {nameof(ServiceType)} but got {type}"
-                })
-            };
+                    ServiceType.GeoIP => new OkObjectResult(await ServiceProcessor.Process<GeoIpModel>(address, type)),
+                    ServiceType.RDAP => new OkObjectResult(await ServiceProcessor.Process<RdapModel>(address, type)),
+                    ServiceType.ReverseDNS =>
+                        new OkObjectResult(await ServiceProcessor.Process<IPHostEntry>(address, type)),
+                    ServiceType.SslLabs => new OkObjectResult(
+                        await ServiceProcessor.Process<SslLabsModel>(address, type)),
+                    ServiceType.Ping => new OkObjectResult(await ServiceProcessor.Process<PingModel>(address, type)),
+                    _ => new BadRequestObjectResult(new ServiceResult<string>
+                    {
+                        Status = ServiceStatus.Bad,
+                        Type = type,
+                        ErrorMessage = $"Invalid type passed in. Expected a {nameof(ServiceType)} but got {type}",
+                        WorkerId = Environment.MachineName,
+                    })
+                };
+            }
+            catch (Exception e)
+            {
+                _logger.LogCritical(e, $"Unknown error in worker for {type}");
+                return StatusCode(StatusCodes.Status500InternalServerError, new ServiceResult<object>
+                {
+                    ErrorMessage = $"Unknown Error from worker {e}",
+                    Status = ServiceStatus.Error,
+                    WorkerId = Environment.MachineName,
+                });
+            }
         }
     }
 }
