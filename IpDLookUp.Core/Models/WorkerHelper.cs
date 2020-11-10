f553c761b9a1ac95810322ab6f31d55ed1a3f5fd
Caleb <caleb.ukle@yahoo.com>
2020-11-10 06:30:47 -0600

    ensure success

--- IpDLookUp.Core/Models/WorkerHelper.cs
+++ IpDLookUp.Core/Models/WorkerHelper.cs
@@ -52,12 +52,13 @@ namespace IPdLookUp.Core.Models
 
         private static async Task<AppResult> Send<TModel>(string url, ServiceType type)
         {
+            var res = await _client.GetAsync(url);
 
-                var res = await _client.GetAsync(url);
-                var content = await res.Content.ReadAsStringAsync();
+            res.EnsureSuccessStatusCode();
+            var content = await res.Content.ReadAsStringAsync();
 
-                var body = JsonSerializer.Deserialize<ServiceResult<TModel>>(content);
-                return SetItemIntoResult(body);
+            var body = JsonSerializer.Deserialize<ServiceResult<TModel>>(content);
+            return SetItemIntoResult(body);
             // }
             // catch (Exception e)
             // {
